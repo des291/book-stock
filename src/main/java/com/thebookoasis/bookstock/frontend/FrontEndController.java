ac2c10f583c86d9c84206da4b381106a664eb169
Des <des.grainger@proton.me>
2024-03-28 20:40:12 +0000

    Updated home page and added page for all books.

--- src/main/java/com/thebookoasis/bookstock/frontend/FrontEndController.java
+++ src/main/java/com/thebookoasis/bookstock/frontend/FrontEndController.java
@@ -4,15 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.thebookoasis.bookstock.book.Book;
+import com.thebookoasis.bookstock.book.BookController;
 import com.thebookoasis.bookstock.book.BookRepository;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class FrontEndController {
 
     private final BookRepository bookRepository;
+    private final BookController bookController;
 
-    public FrontEndController(BookRepository bookRepository) {
+    public FrontEndController(BookRepository bookRepository, BookController bookController) {
         this.bookRepository = bookRepository;
+        this.bookController = bookController;
     }
 
     @GetMapping("/")
@@ -23,6 +32,25 @@ public class FrontEndController {
         model.addAttribute("authorCount", authorCount);
         int genreCount = bookRepository.countGenres();
         model.addAttribute("genreCount", genreCount);
-        return "layout";
+        return "index";
     }
+
+    @GetMapping("/all")
+    public String allBooks(Model model) {
+        List<Book> books = bookRepository.findAll();
+        model.addAttribute("books", books);
+        return "all";
+    }
+
+    @GetMapping("/create")
+    public String createBook(Model model) {
+        return "create";
+    }
+
+    @PostMapping("/create")
+    public String postBook(@RequestBody String entity) {
+
+        return entity;
+    }
+
 }
